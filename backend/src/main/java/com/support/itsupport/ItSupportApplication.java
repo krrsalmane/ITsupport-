92b9f90386a593a4badcc2f50e35f93d4fbe6e06
krrsalmane <karrounsalmane@gmail.com>
2025-06-11 11:45:33 +0100

    project structure

---
+++ backend/src/main/java/com/support/itsupport/ItSupportApplication.java
@@ -0,0 +1,13 @@
+package com.support.itsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItSupportApplication.class, args);
+    }
+
+}
