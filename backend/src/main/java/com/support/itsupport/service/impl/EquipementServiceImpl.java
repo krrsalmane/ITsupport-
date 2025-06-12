b85e12e4dd78057f3674c331b4f87f8925b37394
stouf <ouadad.mustapha01@gmail.com>
2025-06-12 17:33:33 +0100

    Termener parti equipement

--- backend/src/main/java/com/support/itsupport/service/impl/EquipementServiceImpl.java
+++ backend/src/main/java/com/support/itsupport/service/impl/EquipementServiceImpl.java
@@ -1,4 +1,38 @@
 package com.support.itsupport.service.impl;
 
-public class EquipementServiceImpl {
+import com.support.itsupport.entity.Equipement;
+import com.support.itsupport.repository.EquipementRepository;
+import com.support.itsupport.service.EquipementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class EquipementServiceImpl implements EquipementService {
+    @Autowired
+    private EquipementRepository equipementRepository;
+
+    @Override
+    public Equipement ajouterEquipement(Equipement equipement) {
+        return equipementRepository.save(equipement);
+    }
+
+    @Override
+    public Equipement modifierEquipement(Long id, Equipement equipement) {
+        Equipement existant = equipementRepository.findById(id).orElseThrow();
+        existant.setNom(equipement.getNom());
+        existant.setType(equipement.getType());
+        existant.setEtat(equipement.getEtat());
+        return equipementRepository.save(existant);
+    }
+
+    @Override
+    public void supprimerEquipement(Long id) {
+        equipementRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Equipement> getAllEquipements() {
+        return equipementRepository.findAll();
+    }
 }
