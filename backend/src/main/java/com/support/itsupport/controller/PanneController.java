5eaf896923d8a5a4f81f94cd777cd98d63e6bf91
krrsalmane <karrounsalmane@gmail.com>
2025-06-12 15:14:07 +0100

    ajouter un Panne

--- backend/src/main/java/com/support/itsupport/controller/PanneController.java
+++ backend/src/main/java/com/support/itsupport/controller/PanneController.java
@@ -1,4 +1,35 @@
 package com.support.itsupport.controller;
 
+import com.support.itsupport.entity.Panne;
+import com.support.itsupport.service.PanneService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/pannes")
+@CrossOrigin("*") // Allows frontend to connect
 public class PanneController {
+
+    private final PanneService panneService;
+
+    public PanneController(PanneService panneService) {
+        this.panneService = panneService;
+    }
+
+
+
+    @GetMapping("/{id}")
+    public Optional<Panne> getPanneById(@PathVariable Long id) {
+        return panneService.getPanneById(id);
+    }
+
+    @PostMapping
+    public Panne createPanne(@RequestBody Panne panne) {
+        return panneService.addPanne(panne);
+    }
+
+
 }
+
