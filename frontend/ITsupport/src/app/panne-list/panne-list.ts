f028de53baa04d7c413575962e8873b1920fcbb8
krrsalmane <karrounsalmane@gmail.com>
2025-06-15 15:21:33 +0100

    create panne-list

--- frontend/ITsupport/src/app/panne-list/panne-list.ts
+++ frontend/ITsupport/src/app/panne-list/panne-list.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-panne-list',
-  imports: [],
+  standalone: true,
   templateUrl: './panne-list.html',
-  styleUrl: './panne-list.css'
+  styleUrls: ['./panne-list.css']
 })
-export class PanneList {
+export class PanneListComponent implements OnInit {
+  pannes: any[] = [];
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    this.fetchPannes();
+  }
+
+  fetchPannes() {
+    this.http.get<any[]>('/api/pannes').subscribe(data => {
+      this.pannes = data;
+    });
+  }
+
+  deletePanne(id: number) {
+    this.http.delete(`/api/pannes/${id}`).subscribe(() => {
+      this.fetchPannes(); // Refresh the list after deleting
+    });
+  }
+
+  editPanne(id: number) {
+    this.router.navigate(['/pannes/edit', id]);
+  }
+
+  addPanne() {
+    this.router.navigate(['/pannes/add']);
+  }
 }
-
